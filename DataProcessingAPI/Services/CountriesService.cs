67186824f9600a1057c43287526d62edb5eee1ed
Nazar Yuras <nyura@softserveinc.com>
2023-06-21 08:39:28 +0300

    UC#1 Create endpoint

--- DataProcessingAPI/Services/CountriesService.cs
+++ DataProcessingAPI/Services/CountriesService.cs
@@ -6,14 +6,17 @@ namespace DataProcessingAPI.Services
 {
     public class CountriesService : ICountriesService
     {
-        public const string RESTCountriesAPIUrl = "https://restcountries.com/v3.1/all";
-        public CountriesService() { }
+        private readonly IConfiguration _configuration;
+        public CountriesService(IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
 
         public async Task<ServiceResult<List<Country>>> GetCountries()
         {
             var client = new HttpClient();
 
-            var response = await client.GetAsync(RESTCountriesAPIUrl);
+            var response = await client.GetAsync(_configuration.GetValue<string>("AppSettings:RESTCountriesAPIUrl"));
             if (response.IsSuccessStatusCode)
             {
                 var content = await response.Content.ReadAsStringAsync();
